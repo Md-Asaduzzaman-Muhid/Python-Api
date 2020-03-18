d169c70167c6831c9c69d50893635269ccbed713
MD-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-03-18 23:34:24 +0600

    mysql

--- Flask-User-Api/app.py
+++ Flask-User-Api/app.py
@@ -1,36 +1,49 @@
-from flask import Flask, render_template,jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-import os
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+engine = create_engine('mysql://root:Muhid2277@localhost/prottasha_users')
+engine.execute('CREATE TABLE "EX1" ('
+               'id INTEGER NOT NULL,'
+               'name VARCHAR, '
+               'PRIMARY KEY (id));')
+# from flask import Flask, render_template,jsonify, request
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_marshmallow import Marshmallow
+# import os
 
 
 
-#Init App
-app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+# #Init App
+# app = Flask(__name__)
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-@app.route('/', methods = ['GET'])
-def get():
-	return jsonify({'you sent':"some_json"})
+# @app.route('/', methods = ['GET'])
+# def get():
+# 	return jsonify({'you sent':"some_json"})
 
 
-#Database 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+# #Database 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Muhid2277@localhost/prottasha_users'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-ma = Marshmallow(app)
+# ma = Marshmallow(app)
 
-# engine = SQLAlchemy('mysql://root:Muhid2277@localhost/prottasha_users')
-# connection = engine.connect()
+# class Employee(db.Model):
+# 	id = db.Column(db.Integer, primary_key = True)
+# 	username = db.Column(db.String(60), unique = True)
+# 	email = db.Column(db.String(60), unique = True)
 
-#Run Server
-if __name__ == '__main__':
-	app.run(debug=True)
+# # engine = SQLAlchemy('mysql://root:Muhid2277@localhost/prottasha_users')
+# # connection = engine.connect()
 
+# def __init__(self,username,email):
+# 	self.username = username
+# 	self.email = email
 
+# #Run Server
+# if __name__ == '__main__':
+# 	app.run(debug=True)
 
 
 
