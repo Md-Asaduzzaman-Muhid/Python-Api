b6597a5d63204c0a35f1a1e4823b60decb3c95d9
MD-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-03-21 01:23:33 +0600

    update

--- Flask-User-Api/appRestful.py
+++ Flask-User-Api/appRestful.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Muhid2277@localhost/prottasha_users'
+db = SQLAlchemy(app)
+
 class Hello(Resource):
 	def get(self):
-		return {'Hello' : 'World'}
+		return {'Hello' : db}
 	def post(self):
 		some_jeson = request.get_json()
 		return {'You Sent' : some_jeson}, 201
