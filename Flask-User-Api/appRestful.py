cc2e74a4530e390661bb2c0372058c7376525bd2
MD-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-03-24 23:47:29 +0600

    update db

--- Flask-User-Api/appRestful.py
+++ Flask-User-Api/appRestful.py
@@ -8,9 +8,17 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Muhid2277@localhost/prottasha_users'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+	__tablename__ = 'user'
+	id = db.Column('id', db.Integer, primary_key= True)
+	name = db.Column('name', db.String(50))
+	email = db.Column('email', db.String(50))
+	phone = db.Column('phone', db.String(25))
+
+
 class Hello(Resource):
 	def get(self):
-		return {'Hello' : db}
+		return {'Hello' : "db"}
 	def post(self):
 		some_jeson = request.get_json()
 		return {'You Sent' : some_jeson}, 201
